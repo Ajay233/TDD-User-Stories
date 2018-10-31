d315d366f19e524dbac7d2f454cf5672af43a765
Ajay <ajaymungur@hotmail.com>
2018-10-31 12:51:42 +0000

    multiply returns result test failed, fixed and passed

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,6 +2,6 @@ require 'calculator'
 
 describe Calculator do
   it 'calculator object can communicate with #multiply' do
-  expect(subject.multiply(3, 5)).to eq nil
+  expect(subject.multiply(3, 5)).to eq 15
   end
 end

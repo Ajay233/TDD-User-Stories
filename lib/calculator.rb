877d3a74742c714785f330afa2d36bf72f5a69f5
Ajay <ajaymungur@hotmail.com>
2018-10-31 12:02:26 +0000

    commit 2 - Method test failed, fixed and passed

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,4 @@
 class Calculator
-  
+  def multiply
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,7 @@
 require 'calculator'
 
 describe Calculator do
-  
+  it 'calculator object can communicate with #multiply' do
+  expect(subject).to respond_to(:multiply)
+  end
 end
